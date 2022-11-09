1428390f69b8232265cfcde6a0d6c87edf75afdb
faheemjan5000 <faheemjan500@gmail.com>
2022-11-09 15:30:54 +0100

    first commit

---
+++ src/test/java/faheem/microservices/jdbc/spring/boot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package faheem.microservices.jdbc.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
